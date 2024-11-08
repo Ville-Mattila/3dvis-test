267963860db5cd4604da9e2e01806f7632600362
Ville-Mattila <ville.mattila@ensemble.fi>
2024-11-08 11:12:59 +0200

    Update - src/managers/CameraManager.js

--- src/managers/CameraManager.js
+++ src/managers/CameraManager.js
@@ -4,19 +4,16 @@ import { gsap } from 'gsap';
 
 export class CameraManager
 {
-    constructor(sceneManager, controls, camera, scroll)
+    constructor(sceneManager, controls, camera)
     {
-        this.scroll = scroll;
+
         this.sceneManager = sceneManager;
         this.controls = controls;
         this.camera = camera;
-        this.busy = false;
 
         // Function to navigate to a scene zone by index
         this.goToSceneZoneByIndex = (index) =>
         {
-            if (this.busy) return;
-            this.busy = (true);
 
             const sceneZone = this.sceneManager.waypoints[ index ];
             if (!sceneZone)
@@ -32,7 +29,6 @@ export class CameraManager
         this.goToSceneZoneByName = (name) =>
         {
 
-            if (this.busy) return;
             if (!this.camera) return;
             if (!this.sceneManager) return;
 
@@ -53,7 +49,6 @@ export class CameraManager
         {
             if (!sceneZone) return;
             if (sceneZone.index < 0) return;
-            if (!this.scroll) return;
 
             const position = sceneZone.camera.anchor?.position;
 
@@ -61,18 +56,9 @@ export class CameraManager
 
             const target = sceneZone.camera.targetPosition;
 
-            const newScrollOffset = sceneZone.index / (this.sceneManager.sceneZones.length - 1);
-            const scrollTarget = this.scroll.el;
-            const scrollTop = (scrollTarget.scrollHeight - scrollTarget.clientHeight) * newScrollOffset;
-
-            scrollTarget.scrollTo({ top: scrollTop });
-
             if (this.controls === undefined || this.controls === null) return;
 
-            this.controls.setLookAt(...position, ...target, true).then(() =>
-            {
-                this.busy = false;
-            });
+            this.controls.setLookAt(...position, ...target, true);
 
             const tl = gsap.timeline();
 
@@ -107,48 +93,12 @@ export class CameraManager
         }
 
 
-        // Function to handle scrolling
-        this.scrollHandler = () =>
-        {
-
-            if (!this.scroll) return;
-            if (this.busy) return;
-            if (this.scroll.delta < .00004) return;
-
-            const scaledScrollOffset = this.scroll.offset * (this.sceneManager.waypoints.length - 1);
-            const currentZoneIndex = Math.floor(scaledScrollOffset);
-            const nextZoneIndex = Math.ceil(scaledScrollOffset);
-            const currentZone = this.sceneManager.waypoints[ currentZoneIndex ];
-            const nextZone = this.sceneManager.waypoints[ nextZoneIndex ];
-
-            if (!currentZone || !nextZone) return;
-
-            const percent = (scaledScrollOffset % 1);
-
-            // Use slerp to interpolate camera position and target
-            const cameraPosition = currentZone.camera.anchor.position.clone().lerp(nextZone.camera.anchor.position, percent);
-            const cameraTarget = currentZone.camera.targetPosition.clone().lerp(nextZone.camera.targetPosition, percent);
-
-            this.controls.setLookAt(...cameraPosition, ...cameraTarget, true);
-
-            if ("fov" in currentZone.camera.anchor)
-            {
-                this.controls.camera.fov = basicLerp(currentZone.camera.anchor.fov, nextZone.camera.anchor.fov, percent);
-                this.controls.camera.near = basicLerp(currentZone.camera.anchor.near, nextZone.camera.anchor.near, percent);
-                this.controls.camera.far = basicLerp(currentZone.camera.anchor.far, nextZone.camera.anchor.far, percent);
-                this.controls.camera.updateProjectionMatrix();
-                this.controls.update(0);
-            }
-
-        };
 
         this.goToSceneZoneByIndex(0);
     }
 
-
     update()
     {
-        this.scrollHandler();
     }
 
 }
