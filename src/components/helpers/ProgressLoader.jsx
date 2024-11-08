0d5d6467644ea04d8d579042490257867b9316ae
Ville-Mattila <ville.mattila@ensemble.fi>
2024-11-08 11:01:51 +0200

    Create - src/components/helpers/ProgressLoader.jsx

---
+++ src/components/helpers/ProgressLoader.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Html, useProgress } from "@react-three/drei";
+import LoadingScreen from "./LoadingScreen.jsx";
+
+export function ProgressLoader({ setIsLoaded })
+{
+    const { active, progress, errors, item, loaded, total } = useProgress();
+
+    const [ percent, setPercent ] = React.useState(0);
+
+    React.useEffect(() =>
+    {
+        setPercent(Math.floor(progress));
+
+        if (progress >= 99 || loaded >= total)
+        {
+            setIsLoaded(true);
+        }
+
+    }, [ progress ]);
+
+
+    return (
+        <>
+            <Html fullscreen>
+                <LoadingScreen progress={percent} />
+            </Html>
+        </>
+    );
+}
